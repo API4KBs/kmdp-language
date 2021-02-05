f3de36678da3f38ab7ed1c620202ee5363a597fe
Davide Sottara <Sottara.Davide@mayo.edu>
2021-02-05 17:50:37 -0600

    [#56034] Improve FHIR STU3 De/Serializer

--- kmdp-knowledge-transrepresentation-core/src/main/java/edu/mayo/kmdp/language/parsers/fhir/stu3/FHIR3Deserializer.java
+++ kmdp-knowledge-transrepresentation-core/src/main/java/edu/mayo/kmdp/language/parsers/fhir/stu3/FHIR3Deserializer.java
@@ -1,13 +1,18 @@
 package edu.mayo.kmdp.language.parsers.fhir.stu3;
 
+import static org.omg.spec.api4kp._20200801.AbstractCarrier.rep;
 import static org.omg.spec.api4kp._20200801.taxonomy.knowledgeoperation.KnowledgeProcessingOperationSeries.Lifting_Task;
 import static org.omg.spec.api4kp._20200801.taxonomy.knowledgeoperation.KnowledgeProcessingOperationSeries.Lowering_Task;
 import static org.omg.spec.api4kp._20200801.taxonomy.krformat.SerializationFormatSeries.JSON;
 import static org.omg.spec.api4kp._20200801.taxonomy.krformat.SerializationFormatSeries.XML_1_1;
 import static org.omg.spec.api4kp._20200801.taxonomy.krformat.SerializationFormatSeries.asEnum;
 import static org.omg.spec.api4kp._20200801.taxonomy.krlanguage.KnowledgeRepresentationLanguageSeries.FHIR_STU3;
+import static org.omg.spec.api4kp._20200801.taxonomy.parsinglevel.ParsingLevelSeries.Abstract_Knowledge_Expression;
+import static org.omg.spec.api4kp._20200801.taxonomy.parsinglevel.ParsingLevelSeries.Encoded_Knowledge_Expression;
+import static org.omg.spec.api4kp._20200801.taxonomy.parsinglevel.ParsingLevelSeries.Serialized_Knowledge_Expression;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
 import edu.mayo.kmdp.language.parsers.AbstractDeSerializeOperator;
 import java.util.Arrays;
 import java.util.List;
@@ -15,7 +20,7 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.UUID;
 import javax.inject.Named;
-import org.omg.spec.api4kp._20200801.AbstractCarrier;
+import org.hl7.fhir.dstu3.model.Resource;
 import org.omg.spec.api4kp._20200801.id.SemanticIdentifier;
 import org.omg.spec.api4kp._20200801.services.KPOperation;
 import org.omg.spec.api4kp._20200801.services.KPSupport;
@@ -32,6 +37,9 @@ public class FHIR3Deserializer extends AbstractDeSerializeOperator {
 
   public static final UUID id = UUID.fromString("606717df-3f8d-49ee-9d1b-5d2cc1edff55");
   public static final String version = "1.0.0";
+
+  IParser jsonParser = FhirContext.forDstu3().newJsonParser();
+  IParser xmlParser = FhirContext.forDstu3().newXmlParser();
 
   public FHIR3Deserializer() {
     setId(SemanticIdentifier.newId(id,version));
@@ -62,12 +70,14 @@ public class FHIR3Deserializer extends AbstractDeSerializeOperator {
     switch (asEnum(carrier.getRepresentation().getFormat())) {
       case JSON:
         return carrier.asString()
-            .map(str -> AbstractCarrier.ofAst(FhirContext.forDstu3().newJsonParser()
-                .parseResource(str)));
+            .map(jsonParser::parseResource)
+            .map(ast ->
+                newVerticalCarrier(carrier, Abstract_Knowledge_Expression, rep(FHIR_STU3), ast));
       case XML_1_1:
         return carrier.asString()
-            .map(str -> AbstractCarrier.ofAst(FhirContext.forDstu3().newXmlParser()
-                .parseResource(str)));
+            .map(xmlParser::parseResource)
+            .map(ast ->
+                newVerticalCarrier(carrier, Abstract_Knowledge_Expression, rep(FHIR_STU3), ast));
       default:
         return Optional.empty();
     }
@@ -84,13 +94,25 @@ public class FHIR3Deserializer extends AbstractDeSerializeOperator {
   @Override
   public Optional<KnowledgeCarrier> innerEncode(KnowledgeCarrier carrier,
       SyntacticRepresentation into, Properties properties) {
-    return Optional.empty();
+    return carrier.asBinary()
+        .map(str -> newVerticalCarrier(carrier, Encoded_Knowledge_Expression, into, str));
   }
 
   @Override
   public Optional<KnowledgeCarrier> innerExternalize(KnowledgeCarrier carrier,
       SyntacticRepresentation into, Properties properties) {
-    return Optional.empty();
+    switch (asEnum(into.getFormat())) {
+      case JSON:
+        return carrier.as(Resource.class)
+            .map(jsonParser::encodeResourceToString)
+            .map(str -> newVerticalCarrier(carrier, Serialized_Knowledge_Expression, into, str));
+      case XML_1_1:
+        return carrier.as(Resource.class)
+            .map(xmlParser::encodeResourceToString)
+            .map(str -> newVerticalCarrier(carrier, Serialized_Knowledge_Expression, into, str));
+      default:
+        return Optional.empty();
+    }
   }
 
   @Override
